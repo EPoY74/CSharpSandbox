78ca44d71120e13901f026cd580976029ee20c06
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-04-25 17:47:17 +1000

    feat(core): Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+var date = DateTime.Now;
+var dayOfWeek = date.DayOfWeek;
+Console.WriteLine($"{dayOfWeek}");
+Console.WriteLine($"{date.ToString(CultureInfo.CurrentCulture)}");
+Console.WriteLine(CultureInfo.CurrentCulture);
